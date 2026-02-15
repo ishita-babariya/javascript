c7c3deb4029f8f9654cb348be984fbb107ee733c
ishita-babariya <ishita.babariya@uniqueconsumerservices.com>
2026-02-15 12:10:39 +0000

    complete datatype

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3); --> we are using nodejs not browser
+
+// Primitive Datatype
+
+// number->2 to power 53
+// bigint
+// string -> ""
+// boolean -> true / false
+// null -> standalone value
+// undefine
+// symbol -> Unique
+
+// object
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,9 +1,19 @@
 const accountID=14452;
 let accountEmail="test@gmail.com"
-var accountPassword="123456"
+var accountPassword=123456
 accountCity="Ahemdabad"
+let accountState
+
+accountEmail="ishita@gmail.com"
+accountPassword=3245
+accountCity="Junagadh"
 
 
 // accountID=8596
 // console.log(accountID); -> not change const variable
-console.table([accountEmail,accountPassword,accountCity])
+
+/* prefer not use var
+beacuse of issue in block scop and functional scope
+ */
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
