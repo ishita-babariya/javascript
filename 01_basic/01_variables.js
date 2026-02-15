4a6635210c77e0b8d110f770c05a93a0322895b1
ishita-babariya <ishita.babariya@uniqueconsumerservices.com>
2026-02-15 11:21:32 +0000

    variable file

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accountID=14452;
+let accountEmail="test@gmail.com"
+var accountPassword="123456"
+accountCity="Ahemdabad"
+
+
+// accountID=8596
+// console.log(accountID); -> not change const variable
+console.table([accountEmail,accountPassword,accountCity])
