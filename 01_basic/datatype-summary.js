72618c990e389103aa673d6cd80666f66b286d03
ishita-babariya <ishita.babariya@uniqueconsumerservices.com>
2026-02-19 03:10:05 +0000

    datatype summary

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,28 @@
+//Primitive
+// 7 type - String , Number , Boolean , null , undefined , Symbol , BigInt
+
+// Ex of Symbol
+
+const id=Symbol("1234")
+const isValue=Symbol("1234")
+
+// console.log(id===isValue);
+const exOfBigInt=1234567896546545655n;
+
+
+//Non-primitive(refreance datatype)
+//Array , Object , Function
+ const ExOfArray=["Abc","DFG","JIF"];
+ let ExOfObj={
+    name:"ishita",
+    age:21,
+ }
+
+ const ExOfFun=function()
+ {
+    console.log("hello world");
+    
+ }
+ console.log(typeof exOfBigInt);
+ 
+//https://262.ecma-international.org/5.1/#sec-11.4.3
